ae31efcb4d299d39d68fd2941a57f4bd0d1b5d1e
Rusty Conover <rusty@conover.me>
2025-04-18 09:54:17 -0400

    fix: update main branch and fix build

--- src/shell_file_system.hpp
+++ src/shell_file_system.hpp
@@ -2,48 +2,54 @@
 
 #include "duckdb/common/file_system.hpp"
 
-namespace duckdb {
+namespace duckdb
+{
 
-class ShellFileSystem : public FileSystem {
-public:
-	duckdb::unique_ptr<FileHandle> OpenFile(const string &path, FileOpenFlags flags,
-	                                        optional_ptr<FileOpener> opener = nullptr) final;
+	class ShellFileSystem : public FileSystem
+	{
+	public:
+		duckdb::unique_ptr<FileHandle> OpenFile(const string &path, FileOpenFlags flags,
+																						optional_ptr<FileOpener> opener = nullptr) final;
 
-	int64_t Read(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
-	int64_t Write(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
+		int64_t Read(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
+		int64_t Write(FileHandle &handle, void *buffer, int64_t nr_bytes) override;
 
-	int64_t GetFileSize(FileHandle &handle) override;
+		int64_t GetFileSize(FileHandle &handle) override;
 
-	vector<string> Glob(const string &path, FileOpener *opener = nullptr) override {
-		return {path};
-	}
+		vector<OpenFileInfo> Glob(const string &path, FileOpener *opener = nullptr) override
+		{
+			return {path};
+		}
 
-	bool FileExists(const string &filename, optional_ptr<FileOpener> opener = nullptr) override {
-		return false;
-	}
+		bool FileExists(const string &filename, optional_ptr<FileOpener> opener = nullptr) override
+		{
+			return false;
+		}
 
-	void Reset(FileHandle &handle) override;
-	bool OnDiskFile(FileHandle &handle) override {
-		return false;
+		void Reset(FileHandle &handle) override;
+		bool OnDiskFile(FileHandle &handle) override
+		{
+			return false;
+		};
+		bool CanSeek() override
+		{
+			return false;
+		}
+
+		bool CanHandleFile(const string &fpath) override;
+
+		bool IsPipe(const string &filename, optional_ptr<FileOpener> opener) override
+		{
+			return true;
+		}
+		void FileSync(FileHandle &handle) override
+		{
+		}
+
+		std::string GetName() const override
+		{
+			return "ShellFileSystem";
+		}
 	};
-	bool CanSeek() override {
-		return false;
-	}
-
-	bool CanHandleFile(const string &fpath) override;
-
-	bool IsPipe(const string &filename, optional_ptr<FileOpener> opener) override {
-	 	return true;
-  }
-	void FileSync(FileHandle &handle) override {
-
-	}
-
-
-
-	std::string GetName() const override {
-	 	return "ShellFileSystem";
- 	}
-};
 
 } // namespace duckdb
